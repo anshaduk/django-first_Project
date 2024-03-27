3620cba844524f277d2ec4169307adcb30773a0b
Anshad UK <anshadu@gmail.com>
2024-03-27 23:25:08 +0530

    Third commit

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord
 # Create your views here.
 def index(req):
-    dict_content={'view_content':'This text from views.py'}
+    webpg_data=AccessRecord.objects.order_by('date')
+    dict_content={'access_records':webpg_data}
     return render(req,'first_app/index.html',context=dict_content)
